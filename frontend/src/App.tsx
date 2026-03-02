b94e175521d37c92df4544237404e49723759cce
Valentina Guedez Mercado <valentina.guedez.mercado@gmail.com>
2026-03-02 15:55:18 +1100

    Begin creating homepage

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import styles from './Button.module.css';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <div id={styles.websiteLogo}> Study Heights </div>
+        <div> timers </div>
+        <div> login </div>
+        <div> create account </div>
+        <div> study tips </div>
+      </nav>
+      <hr/>
+    </div>
+  )
+}
+
+export default App
