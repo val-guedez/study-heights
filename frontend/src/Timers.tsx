e0cabdb69631bc19770b6f4ba301b9f261898373
Valentina Guedez Mercado <valentina.guedez.mercado@gmail.com>
2026-04-01 09:25:44 +1100

    Start working on timers page

--- frontend/src/Timers.tsx
+++ frontend/src/Timers.tsx
@@ -1,8 +1,35 @@
+import Button from '@mui/material/Button';
+import styles from './styles/Timers.module.css';
+import { useEffect, useState } from 'react';
 
 export default function Timers() {
+  const [timerDisplay, setTimerDisplay] = useState(0);
+  const [showTimer, setShowTimer] = useState(false);
+
+  useEffect(() => {
+    // Timer logic
+  }, [showTimer, timerDisplay]);
+  
   return (
-    <>
-    <h1> THIS IS THE TIMERS PAGE</h1>
-    </>
+    showTimer ? (
+      <>
+      <div id={styles.pageContainer}>
+        <div> {timerDisplay} </div>
+      </div>
+      </>
+    ) : (
+      <>
+      <div id={styles.pageContainer}>
+        <button className={styles.timerButton} onClick={() => handlePomodoro(setShowTimer)}> Pomodoro </button>
+        <button className={styles.timerButton}> Custom Pomodoro </button>
+        <button className={styles.timerButton}> Custom Timer </button>
+
+      </div>
+      </>
+    )
   );
+}
+
+const handlePomodoro = (setShowTimer: (p: boolean) => void) => {
+  setShowTimer(true);
 }
