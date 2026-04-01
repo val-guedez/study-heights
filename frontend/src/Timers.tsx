01d1dde2abf1b1dd0f1936a9332150120a7994d4
Valentina Guedez Mercado <valentina.guedez.mercado@gmail.com>
2026-04-01 16:19:23 +1100

    Create function 20min timer

--- frontend/src/Timers.tsx
+++ frontend/src/Timers.tsx
@@ -1,35 +1,59 @@
-import Button from '@mui/material/Button';
 import styles from './styles/Timers.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+const TIME_IN_MILISECONDS_TO_COUNTDOWN = 60*20*1000;
+const INTERVAL_IN_MILISECONDS = 1000;
 
 export default function Timers() {
-  const [timerDisplay, setTimerDisplay] = useState(0);
   const [showTimer, setShowTimer] = useState(false);
+  const [time, setTime] = useState(TIME_IN_MILISECONDS_TO_COUNTDOWN);
+  const referenceTime = useRef(Date.now());
+
+  const handlePomodoro = () => {
+    setShowTimer(true);
+    referenceTime.current = Date.now();
+  }
 
   useEffect(() => {
-    // Timer logic
-  }, [showTimer, timerDisplay]);
+    console.log("effect ran", { showTimer, time });
+    if (!showTimer) return;  
+    const countDownUntilZero = () => {
+      console.log("tick");
+      const now = Date.now();
+      const interval = now - referenceTime.current;
+      referenceTime.current = now;
+      setTime(prevTime => {
+        console.log("interval:", interval, "prevTime:", prevTime);
+        if (prevTime <= 0) return 0;
+
+        return prevTime - interval;
+      });
+    }
+
+    setTimeout(countDownUntilZero, INTERVAL_IN_MILISECONDS);
+  }, [time, showTimer]);
   
   return (
     showTimer ? (
       <>
       <div id={styles.pageContainer}>
-        <div> {timerDisplay} </div>
+        <div id={styles.timerDisplayContainer}>
+          <div id={styles.timerDisplay}> {(time/60000).toFixed(2)}min </div>
+          <div id={styles.timerControlButtonsContainer}>
+            <button className={styles.timerControlButton}> Pause </button>
+            <button className={styles.timerControlButton}> Continue </button>
+          </div>
+        </div>
       </div>
       </>
     ) : (
       <>
       <div id={styles.pageContainer}>
-        <button className={styles.timerButton} onClick={() => handlePomodoro(setShowTimer)}> Pomodoro </button>
+        <button className={styles.timerButton} onClick={handlePomodoro}> Pomodoro </button>
         <button className={styles.timerButton}> Custom Pomodoro </button>
         <button className={styles.timerButton}> Custom Timer </button>
-
       </div>
       </>
     )
   );
-}
-
-const handlePomodoro = (setShowTimer: (p: boolean) => void) => {
-  setShowTimer(true);
 }
