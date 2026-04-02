1fd3d99027882a13b720ae30a238fef346d4fbb7
Valentina Guedez Mercado <valentina.guedez.mercado@gmail.com>
2026-04-02 16:30:44 +1100

    Change page container min height

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -16,6 +16,19 @@ export default function Home() {
       </div>
       <div id={styles.pixelArt}> *insert pixel art of cat with timer and stationary here* </div>
     </div>
+    <div className={styles.sectionRowContainer} id={styles.anotherContainer}>
+      <div id={styles.pixelArt}> *insert pixel art of cat with timer and stationary here* </div>
+      <div className={styles.textContainer}>
+        <h1 id={styles.titleText}> Ready to reach New Heights? </h1>
+        <ul id={styles.introText}>
+          <li className={styles.dotPoint}> Create custom timers </li>
+          <li className={styles.dotPoint}> Set weekly study goals </li>
+          <li className={styles.dotPoint}> Create a vision board (keeps you motivated!)</li>
+          <li className={styles.dotPoint}> Adopt cute virtual pets </li>
+          <li className={styles.dotPoint}> Beautiful profile customisation </li>
+        </ul>
+      </div>
+    </div>
     </>
   );
 }
